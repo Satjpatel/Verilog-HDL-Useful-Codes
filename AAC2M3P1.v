a589f7351e0ee1d8f249b67af54997cdb9a0d978
Sat J. Patel <satjpatel007@gmail.com>
2020-05-25 09:00:32 +0530

    Update AAC2M3P1.v

--- AAC2M3P1.v
+++ AAC2M3P1.v
@@ -1,26 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //					2 Bit Comparator                                          //
 
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
-//
-// 
-// @file AAC2M3P4.v
-// @brief Application Assignment 2-007 2-bit comparator
-// @version: 1.0 
-// Date of current revision:  @date 2019-06-21  
-// Target FPGA: [Intel Altera MAX10] 
-// Tools used: [Quartus Prime 16.1 or Sigasi] for editing and/or synthesis 
-//             [Modeltech ModelSIM 10.4a Student Edition] for simulation 
-//             [Quartus Prime 16.1]  for place and route if applied
-//             
-//  Functional Description:  This file contains the Verilog which describes the 
-//             FPGA implementation of a 2-bit comparator circuit. 
-//             The inputs are 2-bit vectors A and B 
-//             The output is a scalar Equals that is a 1 iff A and B are equal
-//
-//  Hierarchy:  There is only one level in this simple design.
-//        
 //  Designed by:  @Sat_J_Patel
 //
 //////////////////////////////////////////////////////////////////////////////
