d7ae8f061920f519f7b6113c133a53f023c43658
Sat J. Patel <satjpatel007@gmail.com>
2020-06-28 10:11:24 +0530

    testing the frequency divider circuit

--- tb_clock_freq_divider.v
+++ tb_clock_freq_divider.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   15:19:27 06/27/2020
-// Design Name:   clock_frequency_divider_ckt
+// Create Date:   09:14:00 06/28/2020
+// Design Name:   clock_freq_divider_ckt
 // Module Name:   S:/Github Uploads/BlinkLEDs/tb_clock_freq_divider.v
 // Project Name:  BlinkLEDs
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: clock_frequency_divider_ckt
+// Verilog Test Fixture created by ISE for module: clock_freq_divider_ckt
 //
 // Dependencies:
 // 
@@ -30,27 +30,30 @@ module tb_clock_freq_divider;
 
 	// Outputs
 	wire led;
+	wire [26:0] A;
 
 	// Instantiate the Unit Under Test (UUT)
-	clock_frequency_divider_ckt uut (
+	clock_freq_divider_ckt uut (
 		.clk(clk), 
 		.reset(reset), 
-		.led(led)
+		.led(led), 
+		.A(A)
 	);
-	
-	always      
-	 
-	 #5 clk = ~clk ;
-	 
-	
 
+
+
+	always 
+	#5 clk = ~clk ; 
+	
 	initial begin
 		// Initialize Inputs
 		clk = 0;
 		reset = 1;
+		
 		#10 reset = 0 ; 
+		
 		// Wait 100 ns for global reset to finish
-		#100; 
+		#100;
         
 		// Add stimulus here
 
